55757765fd47fd9b0f03bced20e81ca3e9954672
Aryan Nehete <142139753+aryannehete14@users.noreply.github.com>
2026-04-18 10:05:59 +0530

    Add files via upload

---
+++ Add.java
@@ -0,0 +1,59 @@
+// import java.util.Scanner;
+
+public class Add {
+    // To add 2 numbers using variable declaration & inialization.
+    /* public static void main(String []args){
+        int a;
+        int b;
+        a = 10;
+        b = 20;
+        int sum = (a+b);
+        System.out.println("Sum (a + b) = " + sum);
+    } */
+
+    // To take input from user and print it.
+    /* 
+
+    public static void main(String []args){
+        Scanner sc = new Scanner(System.in);
+        String fname = sc.nextLine();
+
+        //System.out.println("Fname : " + fname);
+        System.out.println("My name is " + fname);
+    } */
+
+    /* public static void main(String []args){
+        Scanner sc = new Scanner(System.in);
+        String name = sc.nextLine();
+        Scanner ab = new Scanner(System.in);
+        String college = ab.nextLine();
+        Scanner bc = new Scanner(System.in);
+        float cgpa = bc.nextFloat();
+
+        /*System.out.println("My name is " + name +'.');
+        System.out.print("I am a student of " + college +'.');
+        System.out.print(" ");
+        System.out.print("My cgpa is " + cgpa +'.'); */
+
+        /* System.out.println("My name is " + name+'.' + " " + "I am a student of " + college+'.' + " " + "My cgpa is " + cgpa+'.'); */
+
+
+         // Type casting - double to float
+    /*public static void main(String []args){
+        double d = 10.55;
+        float f = (float) d ;
+
+
+        System.out.println(f);}
+        */
+
+
+    public static void main(String[] args) {
+        char ch = 65;
+
+        System.out.println(ch);
+        
+    }
+} 
+
+
